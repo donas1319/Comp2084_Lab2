697235196ad20993491956c49149647a4fb18a76
donas1319 <67574045+donas1319@users.noreply.github.com>
2021-02-06 20:21:52 -0500

    Add project files.

---
+++ Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+         
+            return View();
+        }
+
+        public IActionResult Food(string food)
+        {
+            
+            return View();
+        }
+        public IActionResult Tech(string tech)
+        {
+
+            return View();
+        }
+        public IActionResult Sports(string sports)
+        {
+
+            return View();
+        }
+        
+    }
+}
